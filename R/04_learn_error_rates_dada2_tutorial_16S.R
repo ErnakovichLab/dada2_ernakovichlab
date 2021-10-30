138aa12462088bce16b0222b89060ee212e5dfb1
Hannah Holland-Moritz <hhollandmoritz@gmail.com>
2021-10-30 19:01:39 -0400

    adds trial 4 as an alternaive option and runs all trials with error rates based on 10^10 base pairs.

--- R/04_learn_error_rates_dada2_tutorial_16S.R
+++ R/04_learn_error_rates_dada2_tutorial_16S.R
@@ -130,12 +130,14 @@ loessErrfun_mod <- function(trans) {
 errF_1 <- learnErrors(
   filtFs,
   multithread = TRUE,
+  nbases = 1e10,
   errorEstimationFunction = loessErrfun_mod,
   verbose = TRUE
 )
 errR_1 <- learnErrors(
   filtRs,
   multithread = TRUE,
+  nbases = 1e10,
   errorEstimationFunction = loessErrfun_mod,
   verbose = TRUE
 )
@@ -206,6 +208,7 @@ loessErrfun_mod <- function(trans) {
 errF_2 <- learnErrors(
   filtFs,
   multithread = TRUE,
+  nbases = 1e10,
   errorEstimationFunction = loessErrfun_mod,
   verbose = TRUE
 )
@@ -213,6 +216,7 @@ errF_2 <- learnErrors(
 errR_2 <- learnErrors(
   filtRs,
   multithread = TRUE,
+  nbases = 1e10,
   errorEstimationFunction = loessErrfun_mod,
   verbose = TRUE
 )
@@ -283,6 +287,7 @@ loessErrfun_mod <- function(trans) {
 errF_3 <- learnErrors(
   filtFs,
   multithread = TRUE,
+  nbases = 1e10,
   errorEstimationFunction = loessErrfun_mod,
   verbose = TRUE
 )
@@ -292,6 +297,83 @@ errF_3 <- learnErrors(
 errR_3 <- learnErrors(
   filtRs,
   multithread = TRUE,
+  nbases = 1e10,
+  errorEstimationFunction = loessErrfun_mod,
+  verbose = TRUE
+)
+
+#' Trial 4 from Jonalim
+#' alter loess arguments (weights and span and degree, also enforce monotonicity)
+loessErrfun_mod <- function(trans) {
+  qq <- as.numeric(colnames(trans))
+  est <- matrix(0, nrow=0, ncol=length(qq))
+  for(nti in c("A","C","G","T")) {
+    for(ntj in c("A","C","G","T")) {
+      if(nti != ntj) {
+        errs <- trans[paste0(nti,"2",ntj),]
+        tot <- colSums(trans[paste0(nti,"2",c("A","C","G","T")),])
+        rlogp <- log10((errs+1)/tot)  # 1 psuedocount for each err, but if tot=0 will give NA
+        rlogp[is.infinite(rlogp)] <- NA
+        df <- data.frame(q=qq, errs=errs, tot=tot, rlogp=rlogp)
+        
+        # original
+        # ###! mod.lo <- loess(rlogp ~ q, df, weights=errs) ###!
+        # mod.lo <- loess(rlogp ~ q, df, weights=tot) ###!
+        # #        mod.lo <- loess(rlogp ~ q, df)
+        
+        # jonalim's solution
+        # https://github.com/benjjneb/dada2/issues/938
+        mod.lo <- loess(rlogp ~ q, df, weights = log10(tot),degree = 1, span = 0.95)
+        
+        pred <- predict(mod.lo, qq)
+        maxrli <- max(which(!is.na(pred)))
+        minrli <- min(which(!is.na(pred)))
+        pred[seq_along(pred)>maxrli] <- pred[[maxrli]]
+        pred[seq_along(pred)<minrli] <- pred[[minrli]]
+        est <- rbind(est, 10^pred)
+      } # if(nti != ntj)
+    } # for(ntj in c("A","C","G","T"))
+  } # for(nti in c("A","C","G","T"))
+  
+  # HACKY
+  MAX_ERROR_RATE <- 0.25
+  MIN_ERROR_RATE <- 1e-7
+  est[est>MAX_ERROR_RATE] <- MAX_ERROR_RATE
+  est[est<MIN_ERROR_RATE] <- MIN_ERROR_RATE
+  
+  # enforce monotonicity
+  # https://github.com/benjjneb/dada2/issues/791
+  estorig <- est
+  est <- est %>%
+    data.frame() %>%
+    mutate_all(funs(case_when(. < X40 ~ X40,
+                              . >= X40 ~ .))) %>% as.matrix()
+  rownames(est) <- rownames(estorig)
+  colnames(est) <- colnames(estorig)
+  
+  # Expand the err matrix with the self-transition probs
+  err <- rbind(1-colSums(est[1:3,]), est[1:3,],
+               est[4,], 1-colSums(est[4:6,]), est[5:6,],
+               est[7:8,], 1-colSums(est[7:9,]), est[9,],
+               est[10:12,], 1-colSums(est[10:12,]))
+  rownames(err) <- paste0(rep(c("A","C","G","T"), each=4), "2", c("A","C","G","T"))
+  colnames(err) <- colnames(trans)
+  # Return
+  return(err)
+}
+
+# check what this looks like
+errF_4 <- learnErrors(
+  filtFs,
+  multithread = TRUE,
+  nbases = 1e10,
+  errorEstimationFunction = loessErrfun_mod,
+  verbose = TRUE
+)
+errR_4 <- learnErrors(
+  filtRs,
+  multithread = TRUE,
+  nbases = 1e10,
   errorEstimationFunction = loessErrfun_mod,
   verbose = TRUE
 )
@@ -312,21 +394,28 @@ errF_plot1 <- plotErrors(errF_1, nominalQ = TRUE)
 errR_plot1 <-plotErrors(errR_1, nominalQ = TRUE)
 ggsave(plot = errF_plot1, filename = paste0(filtpathF, "/errF_plot1.png"), 
        width = 10, height = 10, dpi = "retina")
-ggsave(plot = errR_plot1, filename = paste0(filtpathF, "/errR_plot1.png"), 
+ggsave(plot = errR_plot1, filename = paste0(filtpathR, "/errR_plot1.png"), 
        width = 10, height = 10, dpi = "retina")
 
 errF_plot2 <- plotErrors(errF_2, nominalQ = TRUE)
 errR_plot2 <-plotErrors(errR_2, nominalQ = TRUE)
 ggsave(plot = errF_plot2, filename = paste0(filtpathF, "/errF_plot2.png"), 
        width = 10, height = 10, dpi = "retina")
-ggsave(plot = errR_plot2, filename = paste0(filtpathF, "/errR_plot2.png"), 
+ggsave(plot = errR_plot2, filename = paste0(filtpathR, "/errR_plot2.png"), 
        width = 10, height = 10, dpi = "retina")
 
 errF_plot3 <- plotErrors(errF_3, nominalQ = TRUE)
 errR_plot3 <-plotErrors(errR_3, nominalQ = TRUE)
 ggsave(plot = errF_plot3, filename = paste0(filtpathF, "/errF_plot3.png"), 
        width = 10, height = 10, dpi = "retina")
-ggsave(plot = errR_plot3, filename = paste0(filtpathF, "/errR_plot3.png"), 
+ggsave(plot = errR_plot3, filename = paste0(filtpathR, "/errR_plot3.png"), 
+       width = 10, height = 10, dpi = "retina")
+
+errF_plot4 <- plotErrors(errF_4, nominalQ = TRUE)
+errR_plot4 <-plotErrors(errR_4, nominalQ = TRUE)
+ggsave(plot = errF_plot4, filename = paste0(filtpathF, "/errF_plot4.png"),
+       width = 10, height = 10, dpi = "retina")
+ggsave(plot = errR_plot4, filename = paste0(filtpathR, "/errR_plot4.png"),
        width = 10, height = 10, dpi = "retina")
 
 
